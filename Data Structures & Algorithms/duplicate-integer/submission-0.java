deb5b63477e4c114bcc02f4875a349ec0008b6c1
Ardhi Gagan <agagan2005@gmail.com>
2026-05-21 21:39:17 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int n : nums){
+            if(hm.containsKey(n)) return true;
+            hm.put(n,0);
+        }
+        return false;
+    }
+}
